73232406d5a8f09e171759695f565fff27c3e96e
lyming99 <36320663+lyming99@users.noreply.github.com>
2024-02-04 16:59:34 +0800

    更新 app_constants.dart
    改为域名

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String apiUrl = "http://106.53.162.153:8080";
+  static const String apiUrl = "http://api.wenznote.com:8080";
 
   static const String success = "success";
 }
